346580565837ce6846a27c201c4aa75dbe63a26e
chanyong <cyjung@gmail.com>
2023-04-16 19:41:36 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,22 @@
 import streamlit as st
 import mysql.connector
-
+import pandas as pd
 
 # Initialize connection.
 # Uses st.cache_resource to only run once.
-@st.cache_resource
+# @st.cache_resource
 def init_connection():
     return mysql.connector.connect(**st.secrets["mysql"])
 
 conn = init_connection()
+curs = conn.cursor()
 
 # Perform query.
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
-@st.cache_data(ttl=600)
-def run_query(query):
-    with conn.cursor() as cur:
-        cur.execute(query)
-        return cur.fetchall()
-
-rows = run_query("select *  from ticket_price_daily;")
-
-# Print results.
-for row in rows:
-    st.write(f"{row[0]} has a :{row[1]}:")
+# @st.cache_data(ttl=600)
+sql = "SELECT * from ticket_price_daily;"
+curs.execute(sql)
+result = curs.fetchall()
+result_df = pd.DataFrame(result)
+# print(result_df)
+st.table(result_df.head())
