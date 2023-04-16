002d65af09f85a0d33e72b06e0aeb93fd10821cb
chanyong <cyjung@gmail.com>
2023-04-16 19:47:04 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,24 @@ import streamlit as st
 import mysql.connector
 import pandas as pd
 
+conn = pymysql.connect(
+    user='cpreview_chanyong',
+    passwd='Moses853!!',
+    host='cpreview.a2hosted.com',
+    db='cpreview_a2wp840',
+    charset='utf8'
+)
+curs = conn.cursor()
+
+
 # Initialize connection.
 # Uses st.cache_resource to only run once.
 # @st.cache_resource
-def init_connection():
-    return mysql.connector.connect(**st.secrets["mysql"])
+# def init_connection():
+#     return mysql.connector.connect(**st.secrets["mysql"])
 
-conn = init_connection()
-curs = conn.cursor()
+# conn = init_connection()
+# curs = conn.cursor()
 
 # Perform query.
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
