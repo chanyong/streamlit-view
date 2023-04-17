d73d9f83588165dabac2813d4ee60a8d5f6d2484
chanyong <cyjung@gmail.com>
2023-04-17 20:38:46 +0900

    Update app.py

--- app.py
+++ app.py
@@ -67,7 +67,10 @@ for idx in range(N):
     chart = alt.Chart(result_df).mark_line().encode(
         x=alt.X('일자:N'),
         y=alt.Y('할인율:Q', scale=alt.Scale(domain=(3, 5))),
-        color=alt.Color("티켓종류:N").legend(orient="top")
+        color=alt.Color("티켓종류:N").legend( orient='none',
+        legendX=130, legendY=-40,
+        direction='horizontal',
+        titleAnchor='middle')
     )
     st.altair_chart(chart, use_container_width=True)
 
