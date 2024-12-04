f0fc8724ab63abffde22e3dd97e4bd56b903496b
chanyong <cyjung@gmail.com>
2024-12-04 22:28:06 +0900

    Update app.py

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ for idx in range(N):
 
     chart = alt.Chart(result_df).mark_line().encode(
         x=alt.X('일자:N'),
-        y=alt.Y('할인율:Q', scale=alt.Scale(domain=(3, 5.5))),
+        y=alt.Y('할인율:Q', scale=alt.Scale(domain=(1, 5.5))),
         color=alt.Color('티켓종류', legend=alt.Legend(
             orient='none',
             direction='horizontal',
